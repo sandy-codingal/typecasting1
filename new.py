1ecab5232d89a296b0b5feef53ad302a03cf4f48
sandy-codingal <sandeepk.codingal@gmail.com>
2026-01-14 13:54:57 +0530

    Add files via upload

---
+++ new.py
@@ -0,0 +1,19 @@
+# typecasting examples in Python
+# Converting integer to float
+num_int = 10    
+num_float = float(num_int)
+print("Integer to Float:", num_float)  # Output: 10.0
+# Converting float to integer
+num_float = 10.5
+num_int = int(num_float)
+print("Float to Integer:", num_int)  # Output: 10
+# Converting string to integer
+num_str = "20"
+num_int = int(num_str)
+print("String to Integer:", num_int)  # Output: 20
+
+
+
+
+
+
